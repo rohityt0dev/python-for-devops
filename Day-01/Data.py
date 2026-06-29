2d702a735a8137c20b40dc4ad7e1d26aef864720
Rohit Tambadkar <rohityt0@gmail.com>
2026-06-29 17:24:33 +0530

    1

---
+++ Day-01/Data.py
@@ -0,0 +1,36 @@
+# Integer Type
+age = 25
+port = 8080
+
+print (age)
+print (port)
+
+# String Type
+name = "Rohit"
+city = "Mumbai"
+Cloud = "AWS"
+
+print (name)
+print (city)
+print (Cloud)
+
+
+# Float Type
+height = 5.1
+weight = 67
+
+print (height)
+print (weight)
+
+# Boolean Type
+server_running = True
+deployment_success = False
+
+print (server_running)
+print (deployment_success)
+
+
+# None Type
+status = None
+
+print (status)
