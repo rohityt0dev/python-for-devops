0ac9e5a7e03f7ced653f89a2ac7a48949ecd18cc
Rohit Tambadkar <rohityt0@gmail.com>
2026-06-30 11:13:11 +0530

    1

---
+++ Day-01/Operators.py
@@ -0,0 +1,41 @@
+# Arithmetic Operators
+a = 10
+b = 5
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+
+# Relational (Comparison) Operators
+
+# print (a == b)
+# print (a != b)
+# print (a > b)
+# print (a < b)
+# print (a >= b)
+# print (a <= b)
+
+# Assignment Operators
+c = 10
+# c += 5
+# c -= 4
+# c *= 2
+# c /= 3
+# c **= 2
+
+# print (c)
+
+# Logical Operators
+
+# Not operator
+# print (not (a > b))
+# print (not (a < b) )
+
+# And operator
+# print (a > b and a < b)
+
+# Or operator
+# print (a > b or a < b)  
